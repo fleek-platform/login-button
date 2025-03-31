e6800b7c96f3e75d89c07cb854935af0284e1ae3
angarita-dev <44899916+angarita-dev@users.noreply.github.com>
2025-03-31 11:59:06 -0500

    Revert "refactor: 💡 Replace build.ts with vite"
    This reverts commit 91a37a97e195199dfe2a87f7efe93fe188567b65.

---
+++ build.ts
@@ -0,0 +1,41 @@
+import { build, BuildOptions } from 'esbuild';
+
+import { parseEnvVarsAsKeyVal } from './src/utils/env';
+import { defined } from './src/defined';
+import pkgJson from './package.json';
+
+import type { Defined } from './src/defined';
+
+const external = [...Object.keys(pkgJson.dependencies)];
+
+const define = parseEnvVarsAsKeyVal<Defined>({
+  defined,
+});
+
+const main = async () => {
+  const buildOptions: BuildOptions = {
+    entryPoints: ['./src/index.ts'],
+    define,
+    bundle: true,
+    platform: 'browser',
+    target: 'esnext',
+    outfile: 'dist/bundle.js',
+    format: 'esm',
+    sourcemap: true,
+    external,
+    minify: true,
+    loader: {
+      '.css': 'text',
+    },
+  };
+
+  try {
+    await build(buildOptions);
+  } catch (error) {
+    console.error('👹 Oops! Failed to bundle for some reason...');
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+main();
